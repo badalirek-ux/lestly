7a6b490d6391ddbd37ddf2e170dd5e42d8cd5e00
badalirek-ux <badalirek@gmail.com>
2026-03-09 18:40:24 +0100

    fix mongodb ssl for render

--- backend/database.py
+++ backend/database.py
@@ -8,16 +8,17 @@ MONGO_URL = os.getenv("MONGO_URL")
 
 client = AsyncIOMotorClient(
     MONGO_URL,
-    tlsAllowInvalidCertificates=True,
     serverSelectionTimeoutMS=30000,
     connectTimeoutMS=30000,
-    retryWrites=True
+    retryWrites=True,
+    tls=True,
+    tlsAllowInvalidCertificates=False,
 )
 
 db = client.lestly
 
-deliveries_collection = db.get_collection("deliveries")
-messages_collection   = db.get_collection("messages")
+deliveries_collection   = db.get_collection("deliveries")
+messages_collection     = db.get_collection("messages")
 riders_collection       = db.get_collection("riders")
 restaurants_collection  = db.get_collection("restaurants")
 
